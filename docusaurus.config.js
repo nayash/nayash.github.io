e9a16195b55140ff8a6d167fa2d4a57add7aa5cd
nayash <lotus.nayak@gmail.com>
2023-09-30 01:23:32 +0530

    trailing slash false

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,6 +47,8 @@ const config = {
         //   //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         // },
         blog: {
+          blogTitle: "Nayash's Brain Dumpy",
+          postsPerPage: 'ALL',
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.

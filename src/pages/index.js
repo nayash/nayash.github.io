e4085288f2ea6b5c0dbc525c286bbdf8f9bcbea5
nayash <lotus.nayak@gmail.com>
2023-09-29 23:40:47 +0530

    about me done

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,9 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
+// import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import ReactMarkdown from 'react-markdown'
+import rehypeRaw from "rehype-raw";
 import styles from './index.module.css';
 
 function HomepageHeader() {
@@ -29,13 +30,28 @@ function HomepageHeader() {
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
+  var src = `This is Asutosh here, a seasoned Software Engineer with a decade of experience in 
+  consultancy, FinTech, EdTech, Ecommerce industries. I have worked in all kinds of environments: huge MNCs with lot of processes to start-ups with "do today or die now" motto and delivered many products and features to production.  \nAreas 
+  of expertise (in decreasing order of proficiency/time spent working) are: Android, Machine Learning, Spring Boot, ReactJs, and .Net (sigh! in a different life).
+  I have worked with Java, Kotlin, Python, Javascript programming languages.  \nThe thoughts expressed on this website are my own and doesn't necessarily reflect the views of any of the organisations I'm affiliated with.  \nI
+  hope you enjoy the ideas expressed here. Click [here](/blog) to visit articles.`
+
   return (
     <Layout
       title={`${siteConfig.title}`}
       description="Personal blog of Asutosh Nayak">
-      <HomepageHeader />
+      <div style={{ display: "flex", justifyContent: "center", padding: "20px"}}>
+        <img src="https://github.com/nayash.png" alt="Me" width={200} />
+      </div>
       <main>
-        <HomepageFeatures />
+        <div style={{ display: "flex", justifyContent: "center"}}>
+          <h4>Howdy Readers!</h4>
+        </div>
+        <div style={{ display: "flex", flexDirection: "row",
+        justifyContent: "center", marginLeft: "10%",
+        marginRight: "10%", lineHeight: "4vh", fontSize: "18px"}}>
+          <ReactMarkdown children={src} rehypePlugins={[rehypeRaw]} />
+        </div>
       </main>
     </Layout>
   );
